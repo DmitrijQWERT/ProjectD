8c0e7eec177147089d3936458e7ff1598f4622bd
DmitrijQWERT <53233144+DmitrijQWERT@users.noreply.github.com>
2021-01-29 07:25:46 +0300

    RS_PAC
    PAcket RS-485 MPC

--- AtmelStudio/Controller/Controller/Controller.cpp
+++ AtmelStudio/Controller/Controller/Controller.cpp
@@ -14,11 +14,6 @@
 #include <string.h>
 #include <stdio.h>
 
-char c_DEVICE_UID[8] = {'C', 'O', 'N', 'T', 'R', 'O', 'L', 'L'};	// CONTROLL
-char c_DEVICE_MAC[4] = {0x01, 0x01, 0x01, 0x01};
-#define DEVICE_UID					c_DEVICE_UID
-#define DEVICE_MAC					c_DEVICE_MAC
-
 #define RS485_SETBIT(x,y)			( x |= ( 1 << y ) )
 #define RS485_CLRBIT(x,y)			( x &= ~( 1 << y ) )
 #define RS485_PORT					PORTD
@@ -35,26 +30,24 @@ char c_DEVICE_MAC[4] = {0x01, 0x01, 0x01, 0x01};
 #define FRAMING_ERROR				( 1 << FE )
 //#define PARITY_ERROR				( 1 << UPE )
 #define DATA_OVERRUN				( 1 << DOR )
-#define RX_ADDRESS_SIZE				4
-//#define RX_DATA_SIZE				16
-#define RX_BUFFER_SIZE				/*1 + RX_ADDRESS_SIZE + RX_DATA_SIZE + 1*/ 8
+#define RX_BUFFER_SIZE				18 // Длина пакета данных
 // Параметры протокола
 #define USART_STARTPACKET 0x00	// Стартовое поле
-char ADRESS_DBK[2] = {0x00, 0x00};	// Адрес ДБК
-char DIEN_DBK[2] = {0x00, 0x00};	// Служебное поле ДБК
-char DIAG_DBK = 0x00;	// Поле диагностики
-char INFO_DBK[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};	// Поле данных
+char ADR_DBK[2] = {0x00, 0x00};	// Адрес ДБК
+char DIEN_DBK[2] = {0x00, 0x00};	// Служебное поле ДБК - заполняется только ДКБ
+char DIAG_DBK = 0x00;	// Поле диагностики - заполняется только ДКБ
+char DAN_DBK[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};	// Поле данных
 char DATA_DBK[4] = {0x00, 0x00, 0x00, 0x00};	// Метка времени
 char CONTROL_DBK[2]	= {0x00, 0x00};	// Контрольное поле
-#define LEN_RS 18	// Длина пакета данных
-#define USART_STOPPACKET 0xBC
+// Полученные данные из линии
+char ex_rx_buffer_ADRESS_DBK[2];
+char ex_rx_buffer_DIEN_DBK[2];
+char ex_rx_buffer_DIAG_DBK;
+char ex_rx_buffer_DAN_DBK[6];
+char ex_rx_buffer_DATA_DBK[4];
+char ex_rx_buffer_CONTROL_DBK[2];
 // This flag is set on USART Receiver buffer overflow
 unsigned char ex_rx_index;
-char ex_rx_buffer_adr[RX_ADDRESS_SIZE];
-char ex_rx_buffer_cmd;
-//char ex_rx_buffer_dat[RX_DATA_SIZE];
-char rx_buffer_dat;
-char ex_rx_buffer_dat1;
 bool ex_rx_buffer_overflow = false;
 bool ex_rx_enable = false;
 bool ex_rx_data_complite = false;
@@ -98,12 +91,8 @@ void UART_Send_Char (char data_tx) ////
 void UART_SendString (char data_tx[])
 {
 	int i;
-	//len = strlen( data_tx );
 	for (i=0; i < RX_BUFFER_SIZE; i++) {
-		//LEDLAMP_ON(1);
 		UART_Send_Char(data_tx[i]);
-		//_delay_ms(50);
-		//LEDLAMP_OFF(1);
 	}
 }
 
@@ -111,157 +100,157 @@ ISR(USART_TXC_vect)
 {
 	RS485_RS; // Установить RE DE в 0. Прием
 }
-
-void USART_SendPacket(char rx_device_mac[4], char rx_buffer_cmd, char rx_buffer_dat /*char rx_buffer_dat[RX_DATA_SIZE]*/)
+void USART_SendPacket(char ADR_DBK[2], char DAN_DBK[6], char DATA_DBK[4], char CONTROL_DBK[2])
 {
 	char tmp_tx_data[RX_BUFFER_SIZE];
-	rx_buffer_dat = 0xCC;
-	sprintf(tmp_tx_data, "%c%c%c%c%c%c%c%c"/*"%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c"*/,	USART_STARTPACKET,
-																	/*
-																	ADRESS_DBK[0],
-																	ADRESS_DBK[1],
+	sprintf(tmp_tx_data, "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",	USART_STARTPACKET,
+																	ADR_DBK[0],
+																	ADR_DBK[1],
 																	DIEN_DBK[0],
 																	DIEN_DBK[1],
 																	DIAG_DBK,
-																	INFO_DBK[0],
-																	INFO_DBK[1],
-																	INFO_DBK[2],
-																	INFO_DBK[3],
-																	INFO_DBK[4],
-																	INFO_DBK[5],
+																	DAN_DBK[0],
+																	DAN_DBK[1],
+																	DAN_DBK[2],
+																	DAN_DBK[3],
+																	DAN_DBK[4],
+																	DAN_DBK[5],
 																	DATA_DBK[0],
 																	DATA_DBK[1],
 																	DATA_DBK[2],
 																	DATA_DBK[3],
 																	CONTROL_DBK[0],
 																	CONTROL_DBK[1]
-																	*/
-																	///*
-																	rx_device_mac[0],
-																	rx_device_mac[1],
-																	rx_device_mac[2],
-																	rx_device_mac[3],
-																	rx_buffer_cmd,
-																	rx_buffer_dat,	
-																	USART_STOPPACKET
-																	//*/
-																	);
-	//LEDLAMP_ON(1);											
+																	);								
 	UART_SendString(tmp_tx_data);
-	//_delay_ms(50);
-	//LEDLAMP_OFF(1);
 }
-
 ISR(USART_RXC_vect)
 {
-	char status, data; ////
-	//unsigned char sub_rx_index;
+	char status, data; //
 	status = UCSRA;
 	data = UDR;
-	if ((status & (FRAMING_ERROR /*| PARITY_ERROR */| DATA_OVERRUN))==0)
+	if ((status & (FRAMING_ERROR | DATA_OVERRUN))==0)
 	{
-
-		if (data == USART_STARTPACKET)	// Получение стартового пакета
+		if ((ex_rx_enable == false) && (data == USART_STARTPACKET))	// Получение стартового пакета
 		{
-			memset(ex_rx_buffer_adr, 0, sizeof(ex_rx_buffer_adr));// Удаление всей информации
-			ex_rx_buffer_cmd = 0;
-			//memset(ex_rx_buffer_dat, 0, sizeof(ex_rx_buffer_dat));
+			// Очистка переменных буфера для приема нового пакета
+			memset(ex_rx_buffer_ADRESS_DBK, 0, sizeof(ex_rx_buffer_ADRESS_DBK));
+			memset(ex_rx_buffer_DIEN_DBK, 0, sizeof(ex_rx_buffer_DIEN_DBK));
+			ex_rx_buffer_DIAG_DBK = 0;
+			memset(ex_rx_buffer_DAN_DBK, 0, sizeof(ex_rx_buffer_DAN_DBK));
+			memset(ex_rx_buffer_DATA_DBK, 0, sizeof(ex_rx_buffer_DATA_DBK));
+			memset(ex_rx_buffer_CONTROL_DBK, 0, sizeof(ex_rx_buffer_CONTROL_DBK));
+			//
 			ex_rx_index=0;
 			ex_rx_enable = true;
 			ex_rx_data_complite = false;
-			
 		}
-		
-		if ( (data == USART_STOPPACKET) && (ex_rx_enable == 1) )
+		if (ex_rx_enable == 1)	// Получение информации
+		{
+			switch (ex_rx_index)
+			{
+				case 1:
+				ex_rx_buffer_ADRESS_DBK[ex_rx_index - 1] = data;
+				break;
+				case 2:
+				ex_rx_buffer_ADRESS_DBK[ex_rx_index - 1] = data;
+				break;
+				case 3:
+				ex_rx_buffer_DIEN_DBK[ex_rx_index - 3] = data;
+				break;
+				case 4:
+				ex_rx_buffer_DIEN_DBK[ex_rx_index - 3] = data;
+				break;
+				case 5:
+				ex_rx_buffer_DIAG_DBK = data;
+				break;
+				case 6:
+				ex_rx_buffer_DAN_DBK[ex_rx_index - 6] = data;
+				break;
+				case 7:
+				ex_rx_buffer_DAN_DBK[ex_rx_index - 6] = data;
+				break;
+				case 8:
+				ex_rx_buffer_DAN_DBK[ex_rx_index - 6] = data;
+				break;
+				case 9:
+				ex_rx_buffer_DAN_DBK[ex_rx_index - 6] = data;
+				break;
+				case 10:
+				ex_rx_buffer_DAN_DBK[ex_rx_index - 6] = data;
+				break;
+				case 11:
+				ex_rx_buffer_DAN_DBK[ex_rx_index - 6] = data;
+				break;
+				case 12:
+				ex_rx_buffer_DATA_DBK[ex_rx_index - 12] = data;
+				break;
+				case 13:
+				ex_rx_buffer_DATA_DBK[ex_rx_index - 12] = data;
+				break;
+				case 14:
+				ex_rx_buffer_DATA_DBK[ex_rx_index - 12] = data;
+				break;
+				case 15:
+				ex_rx_buffer_DATA_DBK[ex_rx_index - 12] = data;
+				break;
+				case 16:
+				ex_rx_buffer_CONTROL_DBK[ex_rx_index - 16] = data;
+				break;
+				case 17:
+				ex_rx_buffer_CONTROL_DBK[ex_rx_index - 16] = data;
+				break;
+			}
+			++ex_rx_index;
+		}
+		if ( (ex_rx_index >= RX_BUFFER_SIZE) && (ex_rx_enable == 1) )	// Прием окончен
 		{
 			ex_rx_enable = 0;
 			ex_rx_data_complite = true;
 		}
-		
-		if (ex_rx_enable == 1)
-		{
-			if ( (ex_rx_index >= 0) && (ex_rx_index <= 4) ) // запись первых 4х данных
-			{
-				ex_rx_buffer_adr[ex_rx_index] = data;
-				if (ex_rx_index == 4)
-				{
-					//if ( !strcat(ex_rx_buffer_adr, DEVICE_MAC) )
-					//{
-					//	ex_rx_enable = 0;
-					//	ex_rx_data_complite = false;
-					//}
-				}
-				++ex_rx_index;
-			} else if (ex_rx_index == 5) // записываем адрес ДБК
-			{
-				ex_rx_buffer_cmd = data;
-				++ex_rx_index;
-			} else if (ex_rx_index > 5)
-			{
-				//sub_rx_index = ex_rx_index - 6;
-				//ex_rx_buffer_dat[sub_rx_index] = data;
-				ex_rx_buffer_dat1 = data;
-				++ex_rx_index;
-			}
-
-			if (ex_rx_index >= RX_BUFFER_SIZE)
-			{
-				ex_rx_enable = false;
-				ex_rx_buffer_overflow=1;
-			}
-			
-		}
-		
 	}
 }
-
-void ExchangeUART(char rx_buffer_cmd, char rx_buffer_dat /*char rx_buffer_dat[RX_DATA_SIZE]*/)
+void ExchangeUART(char ADR_DBK[2], char DAN_DBK[6], char DATA_DBK[4], char CONTROL_DBK[2])
 {
-	//char tmp_rx_buffer[RX_DATA_SIZE];
-	if (rx_buffer_cmd == 0x02) // Активация устройства
-	{
-		//memset(tmp_rx_buffer, 0, sizeof(tmp_rx_buffer));
-		//sprintf(tmp_rx_buffer, "%s", "GOOD!!!");
-		LEDLAMP_ON(1);
-		//USART_SendPacket(DEVICE_MAC, 0xA2, tmp_rx_buffer);
-		USART_SendPacket(DEVICE_MAC, 0x01, rx_buffer_dat);
-		_delay_ms(50);
-		LEDLAMP_OFF(1);
-	}
+	USART_SendPacket(ADR_DBK, DAN_DBK, DATA_DBK, CONTROL_DBK);
 }
-
+char ZeStemp (char DATA_DBK[4])
+{
+	unsigned char j = 0;
+	if (DATA_DBK[j] == 0xFF)
+	{
+		j++;
+		if (j == 4)
+		{
+			memset(DATA_DBK, 0, 4);
+			j = 0;
+		}
+	}
+	return DATA_DBK[j]++;
+}
 int main(void)
 {
-	UART_Init(8); //115200
-	char rx_device_mac[4] = {0xF2, 0xF2, 0xF2, 0xF2};
-		
-	
+	UART_Init(8); // Инициализация UART 115200
+	// Адрес опрашиваемого ДБК
+	ADR_DBK[0] = 0x01;
+	ADR_DBK[1] = 0x5E;
+	// Команда Открыть переезд
+	DAN_DBK[0] = 0x01;  
+	//Установка метки времени
+	DATA_DBK[4] = ZeStemp(DATA_DBK);
+	//
 	DDRD |= ( 1 << PD3);	// Порт на выход
 	RS485_RS; // Установить RE DE в 0. Прием
-	//PORTD = ( 1 << PD3);	// порт на 1
-	
 	DDRC |= ( 1 << PC0) | ( 1 << PC1);	// Порт на выход
-	
 	sei();
-	//ex_rx_data_complite = true;
-	//_delay_ms(1500);
-	//USART_SendPacket(rx_device_mac, 0xA1, ex_rx_buffer_dat1);
-	_delay_ms(15);
-	//memset(ex_rx_buffer_dat, 0, sizeof(ex_rx_buffer_dat));
-	USART_SendPacket(rx_device_mac, 0x01, ex_rx_buffer_dat1);
+	USART_SendPacket(ADR_DBK, DAN_DBK, DATA_DBK, CONTROL_DBK);
 	while(1)
 	{
-		LEDLAMP_ON(0);
-		_delay_ms(50);
-		LEDLAMP_OFF(0);
-		//PORTD = ~( 1 << PD3);
-		//RS485_RS; 
-		//_delay_ms(1);
+		_delay_ms(1);
 		if (ex_rx_data_complite)
 		{
 			ex_rx_data_complite = false;
-			ExchangeUART(ex_rx_buffer_cmd, ex_rx_buffer_dat1);
+			ExchangeUART(ex_rx_buffer_ADRESS_DBK, ex_rx_buffer_DAN_DBK, ex_rx_buffer_DATA_DBK, ex_rx_buffer_CONTROL_DBK);
 		}
-		_delay_ms(15);
 	}
 }
